b9e82b4b3147281d8a6e2f60602635ad8c84a6d8
Ysabel Fernandez <fdzysabel@icloud.com>
2023-05-14 20:39:31 -0400

    added local storage

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-//var timeEl = document.querySelector(".time");
-//var mainEl = document.getElementById("#display");
-var secondsLeft = 75;
+var secondsLeft = 30;
 var startButton = document.querySelector("#generate")
 var questionContainerElement = document.getElementById("question-container")
 var questionElement = document.getElementById("question")
 var answerButtonsElement = document.getElementById("answer-button")
 var scoreElement = document.getElementById("pointScore")
 var shuffledQuestions, currentQuestionIndex
-//var lastQuestion = questionaire.length - 1
 let score = 0
+var total = 4
 
 
 var questionaire = [
@@ -39,17 +37,13 @@ var questionaire = [
     }
 ]
 
-//startButton.addEventListener("click", setTime);
 startButton.addEventListener("click", startQuiz)
-//button.addEventListener("click", checkAnswer)
 
-//document.getElementById("container").addEventListener("click", hide);
 
 function setTime() {
     // Sets interval in variable
     var timeEl = document.querySelector("#display");
-
-    var secondsLeft = 75;
+    var secondsLeft = 30;
     var timerInterval = setInterval(function () {
         secondsLeft--;
         timeEl.textContent = "Time: " + secondsLeft;
@@ -69,10 +63,8 @@ var answerButtonsElement = document.getElementById("question")
 function startQuiz() {
     console.log("start")
     setTime()
-    //startButton.classList.add("hide")
     document.querySelector(".container").classList.add("hide")
     document.querySelector(".question-container").classList.remove("hide")
-    //shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
     setNextQuestion()
 }
@@ -102,7 +94,6 @@ function selectAnswer(event) {
     if (event.target.innerText === questionaire[currentQuestionIndex].correct) {
         console.log("correct")
         score += 1
-        //scoreCounterElement.innerText = + score;
         console.log("score")
         document.querySelector(".display-result").classList.remove("hide")
         document.querySelector(".display-result-again").classList.add("hide")
@@ -119,18 +110,62 @@ function selectAnswer(event) {
     currentQuestionIndex++
     setNextQuestion()
     if (currentQuestionIndex === questionaire.length -1) {
-        console.log(getScore())}
-}
+        getScore();
+        clearInterval() // check 
+}}
 
 function getScore() {
-    //var lastQuestion = questionaire.length - 1
-   // if (lastQuestion) {
         document.querySelector(".question-container").classList.add("hide")
         document.querySelector(".finished-page").classList.remove("hide")
         document.getElementById("pointScore").innerHTML = score;
-      
-     //  var message = scoreCounter / questionaire.length
-    //   message.document.getElementById("pointScore")
 }
+
+var saveButton = document.getElementById("sendScore");
+var savedName = document.getElementById("initials");
+var results = [];
+
+function saveLastScore() {
+  // Save related form data as an object
+  var userScore = {
+    initials: document.getElementById("initials").value, 
+  };
+  // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
+  localStorage.setItem("initials", JSON.stringify(userScore));
+  console.log("hi")
+}
+
+function renderLastScore() {
+   // Use JSON.parse() to convert text to JavaScript object
+  var lastScore = JSON.parse(localStorage.getItem("pointScore"));
+  // Check if data is returned, if not exit out of the function
+  if (lastScore !== null) {
+    console.log("hello")
+     } else {
+        return;
+    }
+}
+
+var highscore = 0;
+var highscore = localStorage.getItem("pointScore");
+
+if(highscore !== null){
+    if (score > highscore) {
+        localStorage.setItem("pointScore", score);      
+    }
+}
+else{
+    localStorage.setItem("pointScore", score);
+}
+  
+
+document.getElementById("sendScore").addEventListener("click", renderLastScore)
+
+
+saveButton.addEventListener("click", function(event) {
+event.preventDefault();
+saveLastScore();
+renderLastScore();
+
+});
 
 
